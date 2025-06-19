9f075b3fcecbf0d45f200647981fab3e24189016
lab_services_student <qxsirdanielgamingxp@gmail.com>
2025-06-19 09:58:55 +0200

    Music Application commit 3 : UI changes and giving function to buttons and adding arrays and variables

--- app/src/main/java/vcmsa/daniel/musicapplication/MainActivity.kt
+++ app/src/main/java/vcmsa/daniel/musicapplication/MainActivity.kt
@@ -1,6 +1,10 @@
+// ST10492718
+// Daniel Johannes Haupt
 package vcmsa.daniel.musicapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +20,80 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        var tvMainScreenTitle = findViewById<TextView>(R.id.tvMainScreenTitle)
+        var btExitMainScreen = findViewById<Button>(R.id.btExitMainScreen)
+        var btAddToPlaylist = findViewById<Button>(R.id.btAddToPlaylist)
+        var btNextMainScreen = findViewById<Button>(R.id.btNextMainScreen)
+        var etSongTitle = findViewById<TextView>(R.id.etSongTitle)
+        var etArtistName = findViewById<TextView>(R.id.etArtistName)
+        var etRatings = findViewById<TextView>(R.id.etRatings)
+        var etComments = findViewById<TextView>(R.id.etComments)
+
+        val song = arrayListOf<String>()
+        val artist = arrayListOf<String>()
+        val ratings = arrayListOf<Int>()
+        val comments = arrayListOf<String>()
+
+        btExitMainScreen.setOnClickListener {
+            finish()
+        }
+        btAddToPlaylist.setOnClickListener {
+
+            song.add(etSongTitle.text.toString())
+            artist.add(etArtistName.text.toString())
+            ratings.add(etRatings.text.toString().toInt())
+            comments.add(etComments.text.toString())
+            etSongTitle.text = ""
+            etArtistName.text = ""
+            etRatings.text = ""
+            etComments.text = ""
+
+
+        }
+
+
+
+        btNextMainScreen.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
